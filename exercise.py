a2bea3e786c58df8a201d671cf2faf4f7ffaadbc
mkatafias <39694527+mkatafias@users.noreply.github.com>
2018-07-05 12:17:14 +0200

    Update exercise.py
    Proposed solution - Marek Katafiasz

--- exercise.py
+++ exercise.py
@@ -2,21 +2,16 @@
 Poniżej znajduje się implementacja CLI (command line interface) do modułu
 turtle, czyli Pythonowego odpowiednika LOGO. Wykorzystano tutaj wzorzec Template
 Method (metoda szablonowa).
-
 W pierwszym, obowiązkowym zadaniu, należy dodać wsparcie dla makr, tak aby można
 było nagrać ciąg komend, a następnie odtworzyć ten sam ciąg przy pomocy
-komendy "playback". W tym celu, należy dodać następujące komendy: 
-
+komendy "playback". W tym celu, należy dodać następujące komendy:
 - record -- rozpoczyna nagrywanie makra
 - stop -- kończy nagrywanie makra
 - playback -- wykonuje makro, tzn. wszystkie komendy po komendzie "record", aż
-  do komendy "stop". 
-
+  do komendy "stop".
 Podpowiedź: Użyj wzorca Command (polecenie).
-
 W drugim, nieobowiązkowym zadaniu, zastanów się, jak można zastosować wzorzec
 Composite (kompozyt) do tych makr i spróbuj zastosować go.
-
 Rozwiązania wysyłamy tak samo, jak prework, tylko że w jednym Pull Requeście.
 '''
 
@@ -58,5 +53,39 @@ class TurtleShell(cmd.Cmd):
         turtle.bye()
         return True
 
+
+class TurtleShellBuffered(TurtleShell):
+    def __init__(self):
+        self.makro = []
+        self.recording = False
+        super().__init__()
+
+    def __getattribute__(self, item):
+        if item.startswith('do_'):
+            if self.recording and item not in ('do_record', 'do_stop', 'do_playback'):
+                return lambda arg: self.__execute(item, arg)
+            else:
+                return super().__getattribute__(item)
+        else:
+            return super().__getattribute__(item)
+
+    def __execute(self, method, args):
+        self.makro.append((method, args))
+
+    def do_record(self, arg):
+        self.makro = []
+        self.recording = True
+
+    def do_playback(self, arg):
+        if self.recording:
+            print("Recording enabled, can't playback in this state. Type stop to stop recording.")
+        else:
+            for method, args in self.makro:
+                super().__getattribute__(method)(args)
+
+    def do_stop(self, arg):
+        self.recording = False
+
+
 if __name__ == '__main__':
-    TurtleShell().cmdloop()    
+    TurtleShellBuffered().cmdloop()
